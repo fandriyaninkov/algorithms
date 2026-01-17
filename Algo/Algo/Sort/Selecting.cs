a51db65d89062c334632bd81afc7e9548cd9a5e1
fandriyaninkov <fandriyaninkov@gmail.com>
2026-01-17 13:36:58 +0300

    Сортировка выбором

---
+++ Algo/Algo/Sort/Selecting.cs
@@ -0,0 +1,30 @@
+﻿namespace Algo.Sort;
+
+/// <summary>Сортировка вставкой</summary>
+public static class Selecting
+{
+    public static void Do(int[] source)
+    {
+        if (source == null || source.Length == 0)
+            return;
+
+        for (int i = 0; i < source.Length; i++)
+        {
+            var min = source[i];
+            var index = i;
+            for (int j = i + 1; j < source.Length; j++)
+            {
+                var el = source[j];
+                if (el < min)
+                {
+                    min = el;
+                    index = j;
+                }
+            }
+
+            var cur = source[i];
+            source[i] = min;
+            source[index] = cur;
+        }
+    }
+}
---
+++ Algo/Tests/AlgoTests/Sort/SelectingTests.cs
@@ -0,0 +1,27 @@
+﻿using Algo.Sort;
+
+namespace Tests.AlgoTests.Sort;
+
+[TestClass]
+public class SelectingTests
+{
+    [TestMethod]
+    public void SelectingTest()
+    {
+        var array = new[] { 156, 141, 35, 94, 88, 61, 111 };
+        var final = new[] { 35, 61, 88, 94, 111, 141, 156 };
+
+        Selecting.Do(array);
+
+        CollectionAssert.AreEqual(final, array);
+    }
+
+    [TestMethod]
+    public void SelectingEmptyTest()
+    {
+        var array = Array.Empty<int>();
+        Selecting.Do(array);
+
+        Assert.AreEqual(0, array.Length);
+    }
+}
