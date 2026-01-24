3b545955220e023272cc4406fbeec5b46a87b588
fandriyaninkov <fandriyaninkov@gmail.com>
2026-01-24 13:16:53 +0300

    LeetCode. 509 фибоначчи

---
+++ Algo/LeetCode/509Fibonacci.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode;
+
+public static class N509Fibonacci
+{
+    public static int Do(int n)
+    {
+        if (n < 0)
+            throw new ArgumentException("Не может быть отрицательное число");
+
+        if (n == 0)
+            return 0;
+        if (n == 1)
+           return 1;
+
+        return Do(n - 1) + Do(n - 2);
+    }
+}
---
+++ Algo/Tests/LeetCode/509FibonacciTests.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode;
+
+namespace Tests.LeetCode;
+
+[TestClass]
+public class N509FibonacciTests
+{
+    [TestMethod]
+    public void FibTest()
+    {
+        var n = 4;
+
+        var res = N509Fibonacci.Do(n);
+
+        Assert.AreEqual(3, res);
+    }
+
+    [TestMethod]
+    public void Fib1Test()
+    {
+        var n = 1;
+
+        var res = N509Fibonacci.Do(n);
+
+        Assert.AreEqual(1, res);
+    }
+
+    [TestMethod]
+    public void Fib0Test()
+    {
+        var n = 0;
+
+        var res = N509Fibonacci.Do(n);
+
+        Assert.AreEqual(0, res);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void FibNegativeTest()
+    {
+        var n = -1;
+
+        var res = N509Fibonacci.Do(n);
+    }
+}
