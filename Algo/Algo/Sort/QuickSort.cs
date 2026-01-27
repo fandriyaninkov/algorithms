c39cdedff66ce91ac797f41a6c7cea531f618f55
fandriyaninkov <fandriyaninkov@gmail.com>
2026-01-27 20:39:21 +0300

    Быстрая сортировка

---
+++ Algo/Algo/Sort/QuickSort.cs
@@ -0,0 +1,22 @@
+﻿namespace Algo.Sort;
+
+/// <summary>
+/// Быстрая сортировка.
+/// Время худший случай O(n^2). Средний случай O(n*log(n))
+/// </summary>
+public static class QuickSort
+{
+    public static int[] Do(int[] nums)
+    {
+        if (nums.Length < 2)
+            return nums;
+
+        var el = nums[0];
+        var less = nums.Skip(1).Where(x => x <= el).ToArray();
+        var greather = nums.Skip(1).Where(x => x > el).ToArray();
+        return Do(less)
+            .Concat([el])
+            .Concat(Do(greather))
+            .ToArray();
+    }
+}
---
+++ Algo/Tests/AlgoTests/Sort/QuickTests.cs
@@ -0,0 +1,58 @@
+﻿using Algo.Sort;
+
+namespace Tests.AlgoTests.Sort;
+
+[TestClass]
+public class QuickTests
+{
+    [TestMethod]
+    public void QuickTest()
+    {
+        var array = new[] { 156, 141, 35, 94, 88, 61, 111 };
+        var final = new[] { 35, 61, 88, 94, 111, 141, 156 };
+
+        var res = QuickSort.Do(array);
+
+        CollectionAssert.AreEqual(final, res);
+    }
+
+    [TestMethod]
+    public void QuickEmptyTest()
+    {
+        var array = Array.Empty<int>();
+        var res = QuickSort.Do(array);
+
+        Assert.AreEqual(0, res.Length);
+    }
+
+    [TestMethod]
+    public void SingleElementTest()
+    {
+        var array = new[] { 42 };
+        var final = new[] { 42 };
+
+        var res = QuickSort.Do(array);
+        CollectionAssert.AreEqual(final, res);
+    }
+
+    [TestMethod]
+    public void SortTest()
+    {
+        var array = new[] { 3, 6, 9, 45, 75 };
+        var final = new[] { 3, 6, 9, 45, 75 };
+
+        var res = QuickSort.Do(array);
+        CollectionAssert.AreEqual(final, res);
+    }
+
+    [TestMethod]
+    public void DuplicateElementsTest()
+    {
+        var array = new[] { 7, 5, 56, 3, 7, 18 };
+        var final = new[] { 3, 5, 7, 7, 18, 56 };
+
+        var res = QuickSort.Do(array);
+
+        CollectionAssert.AreEqual(final, res);
+    }
+}
