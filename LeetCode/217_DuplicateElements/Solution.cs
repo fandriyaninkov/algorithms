7cf6f7073c6dd91e122d437821642427b5a7f6dc
fandriyaninkov <fandriyaninkov@gmail.com>
2026-02-03 21:17:52 +0300

    Leetcode. 217. Contains Duplicate

---
+++ LeetCode/217_DuplicateElements/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode._217_DuplicateElements;
+
+public class Solution
+{
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        var dictionary = new Dictionary<int, int>(nums.Length);
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var val = nums[i];
+            if (dictionary.ContainsKey(val))
+                return true;
+
+            dictionary.Add(val, i);
+        }
+
+        return false;
+    }
+}
