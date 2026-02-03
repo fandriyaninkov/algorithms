7b290f0e0ee5bd478f0f92c7e6efcd6c9f9c1f88
fandriyaninkov <fandriyaninkov@gmail.com>
2026-02-03 21:07:09 +0300

    Leetcode. 1. Two sum

---
+++ Algo/LeetCode/1_TwoSum/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode._1_TwoSum;
+
+public class Solution
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var dictionary = new Dictionary<int, int>(nums.Length);
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var val = nums[i];
+            var x = target - val;
+            if (dictionary.TryGetValue(x, out var ind))
+                return [ind, i];
+
+            dictionary[val] = i;
+        }
+
+        return Array.Empty<int>();
+    }
+}
---
+++ Algo/Tests/LeetCode/1TwoSumTests.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode._1_TwoSum;
+
+namespace Tests.LeetCode;
+
+[TestClass]
+public class N1TwoSumTests
+{
+    [TestMethod]
+    public void FirstTest()
+    {
+        var input = new[] { 2, 7, 11, 15 };
+        var target = 9;
+        var final = new[] { 0, 1 };
+
+        var res = Solution.TwoSum(input, target);
+        CollectionAssert.AreEqual(final, res);
+    }
+
+    [TestMethod]
+    public void SameElementsTest()
+    {
+        var input = new[] { 3, 3 };
+        var target = 6;
+        var final = new[] { 0, 1 };
+
+        var res = Solution.TwoSum(input, target);
+        CollectionAssert.AreEqual(final, res);
+    }
+
+    [TestMethod]
+    public void NegativeElementsTest()
+    {
+        var input = new[] { -3, 4, 3, 90 };
+        var target = 0;
+        var final = new[] { 0, 2 };
+
+        var res = Solution.TwoSum(input, target);
+        CollectionAssert.AreEqual(final, res);
+    }
+}
