90d5432a316dddc171adba102ae90ae559b46f6d
fandriyaninkov <fandriyaninkov@gmail.com>
2026-02-11 21:54:49 +0300

    Leetcode. 733

---
+++ LeetCode/733_FloodFill/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode._733_FloodFill;
+
+public class Solution
+{
+    public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+    {
+        var original = image[sr][sc];
+        if (color == original)
+            return image;
+
+        var queue = new Queue<(int, int)>();
+        queue.Enqueue((sr, sc));
+        while (queue.Count > 0)
+        {
+            var (x, y) = queue.Dequeue();
+            var colorXY = image[x][y];
+            if (colorXY != original)
+                continue;
+            image[x][y] = color;
+            if (CheckInArray(image, x + 1, y, color))
+                queue.Enqueue((x + 1, y));
+            if (CheckInArray(image, x - 1, y, color))
+                queue.Enqueue((x - 1, y));
+            if (CheckInArray(image, x, y + 1, color))
+                queue.Enqueue((x, y + 1));
+            if (CheckInArray(image, x, y - 1, color))
+                queue.Enqueue((x, y - 1));
+        }
+        
+        return image;
+    }
+
+    private bool CheckInArray(int[][] image, int x, int y, int color)
+    {
+        return x >= 0
+            && y >= 0
+            && image.Length - 1 >= x
+            && image[x].Length - 1 >= y 
+            && image[x][y] != color;
+    }
+}
---
+++ Tests/LeetCode/733FloodFill.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode._733_FloodFill;
+
+[TestClass]
+public class N733FloodFill
+{
+    [TestMethod]
+    public void FirstTest()
+    {
+        var image = new int[][] { [1, 1, 1], [1, 1, 0], [1, 0, 1] };
+        var sr = 1;
+        var sc = 1;
+        var color = 2;
+        var sol = new Solution();
+
+        var res = sol.FloodFill(image, sr, sc, color);
+
+    }
+
+    [TestMethod]
+    public void SecondTest()
+    {
+        var image = new int[][] { [0, 0, 0], [0, 0, 0] };
+        var sr = 0;
+        var sc = 0;
+        var color = 0;
+        var sol = new Solution();
+
+        var res = sol.FloodFill(image, sr, sc, color);
+
+    }
+}
