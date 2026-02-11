3a3ac945f7ac94588224b93b98af88941f26c8e1
fandriyaninkov <fandriyaninkov@gmail.com>
2026-02-11 22:09:45 +0300

    Leetcode. 733. Улучшение

--- LeetCode/733_FloodFill/Solution.cs
+++ LeetCode/733_FloodFill/Solution.cs
@@ -10,32 +10,34 @@ public class Solution
 
         var queue = new Queue<(int, int)>();
         queue.Enqueue((sr, sc));
+        int count = 0;
         while (queue.Count > 0)
         {
+            count++;
             var (x, y) = queue.Dequeue();
             var colorXY = image[x][y];
-            if (colorXY != original)
+            if (colorXY != original || colorXY == color)
                 continue;
             image[x][y] = color;
-            if (CheckInArray(image, x + 1, y, color))
+            if (CheckInArray(image, x + 1, y, original))
                 queue.Enqueue((x + 1, y));
-            if (CheckInArray(image, x - 1, y, color))
+            if (CheckInArray(image, x - 1, y, original))
                 queue.Enqueue((x - 1, y));
-            if (CheckInArray(image, x, y + 1, color))
+            if (CheckInArray(image, x, y + 1, original))
                 queue.Enqueue((x, y + 1));
-            if (CheckInArray(image, x, y - 1, color))
+            if (CheckInArray(image, x, y - 1, original))
                 queue.Enqueue((x, y - 1));
         }
         
         return image;
     }
 
-    private bool CheckInArray(int[][] image, int x, int y, int color)
+    private bool CheckInArray(int[][] image, int x, int y, int original)
     {
         return x >= 0
             && y >= 0
             && image.Length - 1 >= x
             && image[x].Length - 1 >= y 
-            && image[x][y] != color;
+            && image[x][y] == original;
     }
 }
--- Tests/LeetCode/733FloodFill.cs
+++ Tests/LeetCode/733FloodFill.cs
@@ -7,25 +7,27 @@ public class N733FloodFill
     public void FirstTest()
     {
         var image = new int[][] { [1, 1, 1], [1, 1, 0], [1, 0, 1] };
+        var expected = new int[][] { [2, 2, 2], [2, 2, 0], [2, 0, 1] };
         var sr = 1;
         var sc = 1;
         var color = 2;
         var sol = new Solution();
 
         var res = sol.FloodFill(image, sr, sc, color);
-
+        CollectionAssert.AreEqual(expected, res);
     }
 
     [TestMethod]
     public void SecondTest()
     {
         var image = new int[][] { [0, 0, 0], [0, 0, 0] };
+        var expected = new int[][] { [0, 0, 0], [0, 0, 0] };
         var sr = 0;
         var sc = 0;
         var color = 0;
         var sol = new Solution();
 
         var res = sol.FloodFill(image, sr, sc, color);
-
+        CollectionAssert.AreEqual(expected, res);
     }
 }
